246bb0edc6e8dbfa54ba1224a273918a5e0e93a1
matti.rintanikkola <matti.rintanikkola@fatswebnet.it>
2013-12-15 14:55:04 +0100

    removed obsolete code...

--- src/main/java/cz/jirutka/rsql/parser/model/ComparisonExpression.java
+++ src/main/java/cz/jirutka/rsql/parser/model/ComparisonExpression.java
@@ -77,12 +77,6 @@ public class ComparisonExpression extends Expression {
     
     @Override
     public String toString() {
-    	String lparen="";
-    	String rparen="";
-    	if (operator==Comparison.IN || operator==Comparison.OUT){
-    		lparen="(";
-    		rparen=")";
-    	}
         return selector + " " + operator + " " + argument;
     }
 

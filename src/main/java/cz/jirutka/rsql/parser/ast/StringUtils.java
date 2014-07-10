a41d2e89f48d8884e54585c76e1988f6a89fa1ed
Jakub Jirutka <jakub@jirutka.cz>
2014-07-11 00:37:28 +0200

    Remove unused method

--- src/main/java/cz/jirutka/rsql/parser/ast/StringUtils.java
+++ src/main/java/cz/jirutka/rsql/parser/ast/StringUtils.java
@@ -39,8 +39,4 @@ abstract class StringUtils {
     public static boolean isBlank(String str) {
         return str == null || str.trim().isEmpty();
     }
-
-    public static boolean isNotBlank(String str) {
-        return ! isBlank(str);
-    }
 }
